c95519b0b486f55efa7c33c13c51d72fe4ecf51c
que <2427491142@qq.com>
2023-05-17 18:57:59 +0800

    将微服务提供和Eureka进行集群配置

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,14 +14,15 @@ import javax.annotation.Resource;
 public class PaymentController {
     @Resource
     private PaymentService paymentService;
-
+    @Value("${server.port}")
+    private String serverPort;
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("插入结果：" + result);
 
         if (result > 0) {
-            return new CommonResult(200,"插入成功",result);
+            return new CommonResult(200,"插入成功,serverPort:"+serverPort,result);
         }else {
             return new CommonResult(404,"插入失败",null);
         }
@@ -32,7 +34,7 @@ public class PaymentController {
         log.info("插入结果：" + payment);
 
         if (payment != null) {
-            return new CommonResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功,serverPort:"+serverPort,payment);
         }else {
             return new CommonResult(404,"查询失败,查询ID为：" + id,null);
         }
